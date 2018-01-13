a4b2f53e7d30a19d67c15669e41bcbb639a68279
adamgradkowski <adam.gradkowski1994@gmail.com>
2018-01-13 16:54:06 +0100

    Added Public/Non Public

--- accounts.py
+++ accounts.py
@@ -10,30 +10,30 @@ class Account:
 		return pytz.utc.localize(utc_time)
 
 	def __init__(self,n,b):
-		self.name = n
-		self.balance = b
-		self.transaction_list = []
-		print("Account created for " + self.name)
+		self.__name = n # Adam: doczytac public / non public : _ / __ / bez
+		self.__balance = b
+		self.__transaction_list = [(Account._current_time(), b)]
+		print("Account created for " + self.__name)
 
 	def show_balance(self):
-		print("Balace is {}".format(self.balance))
+		print("Balace is {}".format(self.__balance))
 
 	def deposit(self, amount):
 		if amount > 0:
-			self.balance += amount
-			self.transaction_list.append((Account._current_time(), amount))
+			self.__balance += amount
+			self.__transaction_list.append((Account._current_time(), amount))
 			#self.transaction_list.append((pytz.utc.localize(datetime.datetime.utcnow()), amount))
 			self.show_balance()
 
 	def withdraw(self,amount):
-		if 0 < amount <= self.balance:
-			self.balance -= amount
-			self.transaction_list.append((Account._current_time(), -amount))
+		if 0 < amount <= self.__balance:
+			self.__balance -= amount
+			self.__transaction_list.append((Account._current_time(), -amount))
 		else:
 			print("The amount must be greater than 0 and no more than acount balance")
 		self.show_balance()
 	def show_transactions(self):
-		for date, amount in self.transaction_list:
+		for date, amount in self.__transaction_list:
 			if amount > 0:
 				trans_type = 'deposited'
 			else:
@@ -48,3 +48,9 @@ if __name__ == '__main__':
 	adam.withdraw(100)
 	adam.withdraw(100)
 	adam.show_transactions()
+
+	ania = Account("Ania", 800)
+	ania.__balance = 200
+	ania.deposit(100)
+	ania._Account__balance = 404
+	ania.show_balance()

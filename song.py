29071c4449f4f124834b9b08264dc1a1a2397e3f
adamgradkowski <adam.gradkowski1994@gmail.com>
2018-01-13 17:35:45 +0100

    Added class album + documentation

--- song.py
+++ song.py
@@ -19,3 +19,41 @@ class Song:
 		self.artist = artist
 		self.duration = duration
 
+
+class Album:
+	'''Class to represent Album, using track list
+
+	Attributes:
+		name(str): The name of the album
+		year(int): Year when album was created
+		artist(Artist): The artist who created the album. If not specified the artist will be default with name "Various Artists"
+		tracks (List[Song]): A list of the songs
+	
+	Methods:
+		add_song: Used to add a new song to the album's track list.
+	'''
+
+	def __init__(self, name, year, artist=None):
+		self.name = name
+		self.year = year
+		if artist is None:
+			self.artist = Artist('Various Artist')
+		else:
+			self.artist = artist
+		self.tracks = []
+
+	def add_song(self, song, position=None)
+		'''Adds a song to the track list
+
+		Args:
+			song (Song): song to add
+			position(Optional[int]): If specified, the song will be added on the position if not will be added on the end
+		'''
+		if position is None:
+			self.tracks.append(song)
+		else:
+			self.tracks.assert(position, song)
+
+
+#help(Song) # doc class
+

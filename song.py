024ca6526eb12af2dda585eaa826703919e4996e
adamgradkowski <adam.gradkowski1994@gmail.com>
2018-01-13 23:00:20 +0100

    Load data + write checkfile

--- song.py
+++ song.py
@@ -76,12 +76,54 @@ def load_data():
 	with open("albums.txt", "r") as albums:
 		for line in albums:
 			#data row should consit of (artist, album, year, song)
-			artist_filed, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
+			artist_field, album_field, year_field, song_field = tuple(line.strip('\n').split('\t'))
 			year_field = int(year_field)
-			print(artist_filed, album_field, year_field, song_field)
+			#print(artist_field, album_field, year_field, song_field)
 
+			if new_artist is None:
+				new_artist = Artist(artist_field)
+			elif new_artist.name != artist_field:
+				new_artist.add_album(new_album)
+				artist_list.append(new_artist)
+				new_artist = Artist(artist_field)
+				new_album = None
+				
+			
+			if new_album is None:
+				new_album = Album(album_field, year_field, new_artist)
+			elif new_album.name != album_field:
+				new_artist.add_album(new_album)
+				new_album = Album(album_field, year_field, new_artist)
+
+			new_song = Song(song_field, new_artist)
+			new_album.add_song(new_song)
+
+		if new_artist is not None:
+			if new_album is not None:
+				new_artist.add_album(new_album)
+			artist_list.append(new_artist)
+
+	return artist_list #Adam: szukaj innego rowiazania tego load'a
+
+
+def create_check_file(artist_list):
+	"""Create a check file to compare with original file"""
+	with open("checkfile.txt", 'w') as checkfile:
+		for new_artist in artist_list:
+			for new_album in new_artist.albums:
+				for new_song in new_album.tracks:
+					print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song), file=checkfile)
 if __name__ == '__main__':
-	load_data()
-
+	artists = load_data()
+	create_check_file(artists)
+	'''for a in artists:
+		print("*" * 80)
+		print(a.name)
+		for i in a.albums:
+			print(i.name)
+			for song in i.tracks:
+				print(song.title)
+		print("*" * 80)
+	'''
 #help(Song) # doc class
 

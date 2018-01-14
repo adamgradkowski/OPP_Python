bc1f78b2e5a333270e1c2db892ee77b78f110dcb
adamgradkowski <adam.gradkowski1994@gmail.com>
2018-01-14 14:38:25 +0100

    More player property

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ print(tim)
 tim.lives -=1
 print(tim)
 tim.lives -=1
+
+print(tim)
+tim.level -=1
+
+print(tim)
+tim.level = 5
+
+print(tim)
+tim.level -=1
+print(tim)
--- player.py
+++ player.py
@@ -2,8 +2,8 @@ class Player(object):
 
 	def __init__(self, name):
 		self.name = name
-		self.lives = 3
-		self.level = 0
+		self._lives = 3
+		self._level = 0
 		self.score = 0
 
 	def _get_lives(self):
@@ -15,8 +15,18 @@ class Player(object):
 		else:
 			print("Lives cannnot be nagative")
 			self._lives = 0
+	def _get_level(self):
+		return self._level
 
+	def _set_level(self,level):
+		if level > 0:
+			delta = level - self._level
+			self.score += delta * 1000
+			self._level = level
+		else:
+			print("Level can't be less than 1")	
 	lives = property(_get_lives, _set_lives)
+	level = property(_get_level, _set_level)
 
 	def __str__(self):
 		return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score {0.score}".format(self)
